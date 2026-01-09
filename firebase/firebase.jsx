8b17970fd154eaab0fd0063f24d138ada3d1a76d
lalit999999 <gujarlalit79@gmail.com>
2026-01-10 00:21:40 +0530

    all files and frontend added in project

---
+++ firebase/firebase.jsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAzE5Da0H3JhKUcCKZwb0Zy8L2h9-6YwQE",
+  authDomain: "app-8b003.firebaseapp.com",
+  databaseURL: "https://app-8b003-default-rtdb.firebaseio.com",
+  projectId: "app-8b003",
+  storageBucket: "app-8b003.firebasestorage.app",
+  messagingSenderId: "790357474054",
+  appId: "1:790357474054:web:ec0b2ccb4d1d05f369bc87"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/assets/images.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import img1 from "../assets/googleicon.svg";
+
+
+export function Googleicon() {
+return <img width={25} src={img1} alt="googleicon" />;
+}
