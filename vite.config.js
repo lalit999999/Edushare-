ee535e9942384d7f49186517fa5838c6243ba6ec
lalit999999 <gujarlalit79@gmail.com>
2026-01-10 00:52:38 +0530

    fixing an issue in deployment

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  darkMode: 'class', 
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    tailwindcss(),
+  ],
+})
