f6db4bf2a0d7f1c613b779e9c5a4837fcad79ca4
lalit999999 <gujarlalit79@gmail.com>
2026-01-10 00:31:35 +0530

    firebase added

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,4 +1,9 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAzE5Da0H3JhKUcCKZwb0Zy8L2h9-6YwQE",
   authDomain: "app-8b003.firebaseapp.com",
@@ -9,5 +14,9 @@ const firebaseConfig = {
   appId: "1:790357474054:web:ec0b2ccb4d1d05f369bc87"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
